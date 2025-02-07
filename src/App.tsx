45ec07bca78526e55884bca85435b25208193170
aram1l7 <arammartirosyann16@gmail.com>
2025-02-07 18:37:25 +0400

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import PDFHighlighter from "./components/PDFHighlighter";
 function App() {
-  return <PDFHighlighter pdfUrl={"/wa-cigna-dental-preventive-policy.pdf"} />;
+  return (
+    <PDFHighlighter
+      pdfUrl={new URL(
+        "/wa-cigna-dental-preventive-policy.pdf",
+        window.location.origin
+      ).toString()}
+    />
+  );
 }
 
 export default App;
